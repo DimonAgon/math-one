126c4d74fbb56dd13abc5b6ed93ebb8efcbac4db
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-03-07 20:47:30 +0200

    Simplified 30th case

--- test/test_simplified_formula_is_same_as_long_one_case_30.py
+++ test/test_simplified_formula_is_same_as_long_one_case_30.py
@@ -19,5 +19,4 @@ def test_initial_formula_is_equal_to_simplified():
     for A in (True, False):
         for B in (True, False):
             for C in (True, False):
-                assert initial_formula(A, B, C) == (
-                            not C and set_difference(A, C) and set_difference(B, C) and (not C or B))
+                assert initial_formula(A, B, C) == (A and set_difference(B, C))
